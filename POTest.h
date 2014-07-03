f5905798bc98dde2c9a3b0a237721a044a6f8d3f
Patrick Odenborg <PatrickOdenborg@Patrick-Odenborgs-MacBook-Pro.local>
2014-07-03 14:09:23 -0600

    added test class

---
+++ POTest.h
@@ -0,0 +1,13 @@
+//
+//  POTest.h
+//  Git Test
+//
+//  Created by Patrick Odenborg on 7/3/14.
+//  Copyright (c) 2014 Patrick Odenborg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface POTest : NSObject
+
+@end
---
+++ POTest.m
@@ -0,0 +1,13 @@
+//
+//  POTest.m
+//  Git Test
+//
+//  Created by Patrick Odenborg on 7/3/14.
+//  Copyright (c) 2014 Patrick Odenborg. All rights reserved.
+//
+
+#import "POTest.h"
+
+@implementation POTest
+
+@end
